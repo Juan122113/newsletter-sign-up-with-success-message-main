266e580effb51822dfe82a15d93157a4c18092a4
Juan122113 <ammiel89@hotmail.com>
2024-07-31 20:34:50 -0300

    Update index.html & main.js

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 let form = document.getElementById("form");
 let emailForm = document.getElementById("email-form");
 let button = document.getElementById("button");
+let bValid = document.getElementById("b-Valid-id");
+let bValidDisplay = bValid.getAttribute("display");
+let bValidStyle = window.getComputedStyle(bValid);
 
 function handleSubmit(e) {
 
     e.preventDefault()
 
-    console.log(e);
+    const data = Object.fromEntries(new FormData(e.target));
 
-    if ((/(^\W+)(\w+)@/i).test(e)) {
-        alert("false")
-    } else {
+    console.log(data);
+
+    if (!(/(^\W+)(\w+)@/i).test(data)) {
+        
         window.location.href = "success.html"
-        alert("true");
+        
+    } else {
+        bValid.style.display = "block";
+        // bValidStyle.display = "block";
+        // bValid.ATTRIBUTE_NODE(screen.style.display) = "block"
+        // window.alert("false")
     }
     // const data = Object.fromEntries(new FormData(e.target));
 
