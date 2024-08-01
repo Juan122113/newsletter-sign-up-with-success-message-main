3b70670dd76695ae64045140d364fc0616ce87ed
Juan122113 <ammiel89@hotmail.com>
2024-08-01 09:18:53 -0300

    Update index.html & main.js

--- main.js
+++ main.js
@@ -1,33 +1,55 @@
 let form = document.getElementById("form");
-let emailForm = document.getElementById("email-form");
+const emailForm = document.getElementById("email-form");
 let button = document.getElementById("button");
-let bValid = document.getElementById("b-Valid-id");
+const bValid = document.getElementById("b-Valid-id");
 let bValidDisplay = bValid.getAttribute("display");
 let bValidStyle = window.getComputedStyle(bValid);
 
-function handleSubmit(e) {
 
-    e.preventDefault()
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-    const data = Object.fromEntries(new FormData(e.target));
-
-    console.log(data);
-
-    if (!(/(^\W+)(\w+)@/i).test(data)) {
-        
+    if (emailForm.checkValidity()) {
         window.location.href = "success.html"
-        
-    } else {
+        console.log("true");
+    } else if (emailForm.checkValidity() == false){
         bValid.style.display = "block";
+        console.log("false");
+    }
+
+})
+
+
+// function handleSubmit(e) {
+
+    // e.preventDefault()
+
+    // const data = Object.fromEntries(new FormData(e.target));
+
+    // console.log(data);
+
+    // if (emailForm.checkValidity()) {
+        // window.location.href = "success.html"
+    // } else {
+        // bValid.style.display = "block";
+    // }
+
+    // if (!(/(^\W+)(\w+)@/i).test(data)) {
+        
+        // window.location.href = "success.html"
+        
+    // } else {
+        // bValid.style.display = "block";
         // bValidStyle.display = "block";
         // bValid.ATTRIBUTE_NODE(screen.style.display) = "block"
         // window.alert("false")
-    }
+    // }
     // const data = Object.fromEntries(new FormData(e.target));
 
     // for (const dat in data)
 
 
-}
+// }
 
-form.addEventListener('submit', handleSubmit);
+// form.addEventListener('submit', handleSubmit);
+
