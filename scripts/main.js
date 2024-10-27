20f3870851087383e5c15c1ef2c13accee2232e5
Juan122113 <ammiel89@hotmail.com>
2024-10-27 00:46:28 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,20 +3,21 @@ const emailForm = document.getElementById("email-form");
 let button = document.getElementById("button");
 const bValid = document.getElementById("b-Valid-id");
 let bValidDisplay = bValid.getAttribute("display");
+let a;
 // let bValidStyle = window.getComputedStyle(bValid);
 
-const styles = {
-    invalid: {
+const emailFormStyles = {
+    invl: {
         borderColor: "var(--Tomato)",
         color: "var(--Tomato)",
         backgroundColor: "hsla(4, 100%, 67%, 0.170)",
-        bValid: 
+        //bValid: 
     },
-    focus: {
+    foc: {
         borderColor: "var(--Grey)",
         color: "var(--Dark-Slate-Grey)",
         backgroundColor: "white",
-        bValid:
+        //bValid:
     }
 }
 
@@ -37,20 +38,48 @@ emailForm.addEventListener("invalid", (e) => {
     emailForm.style.backgroundColor = "hsla(4, 100%, 67%, 0.170)";
 })
 
-emailForm.addEventListener('focus', function() {
-    // console.log('El campo de email tiene el foco.');
+for (let item in emailFormStyles) {
+    
+}
+for (a in emailFormStyles.foc) {
+    // console.log(emailFormStyles.attention);
+    // console.log(a);
+}
+
+function focusState(a, emailFormStyles) {
+    // console.log(a);
+    // for (let a in emailFormStyles.attention) {
+    //     console.log(emailFormStyles.attention);
+    //     console.log(a);
+    // }
     bValid.style.display = "none";
-    emailForm.style.color = "var(--Dark-Slate-Grey)";
-    emailForm.style.borderColor = "var(--Grey)";
-    emailForm.style.backgroundColor = "white";
-});
+    // emailForm.style.color = "var(--Dark-Slate-Grey)";
+    // emailForm.style.borderColor = "var(--Grey)";
+    // emailForm.style.backgroundColor = "white";
+    emailForm.style[a] = emailFormStyles.foc[a];
+}
+
+emailForm.addEventListener('focus', focusState(a, emailFormStyles));
+
+// emailForm.addEventListener('focus', function(a) {
+//     console.log(a);
+//     // console.log('El campo de email tiene el foco.');
+//     bValid.style.display = "none";
+//     emailForm.style.color = "var(--Dark-Slate-Grey)";
+//     emailForm.style.borderColor = "var(--Grey)";
+//     emailForm.style.backgroundColor = "white";
+
+//     // for (let a in emailFormStyles.attention) {
+//     //     console.log(emailFormStyles[attention]);
+//     // }
+// });
 
 form.addEventListener('submit', function(event) {
     
     event.preventDefault();
     
     let emailValue = emailForm.value;
-    console.log(emailValue);
+    // console.log(emailValue);
 
     if (emailForm.checkValidity()) {
         console.log("true");
